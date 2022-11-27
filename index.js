938e6443c2eaa71098125a5f219c8b6214fcc6d9
afruzaakter <afruzaaktersonia1995@gmail.com>
2022-11-27 23:36:42 +0600

    admin panel get and put

--- index.js
+++ index.js
@@ -390,6 +390,34 @@ async function run(){
         res.send(users)
     })
 
+         //-------------- User Admin  put or  update ---------------------
+         app.put('/user/admin/:email', async(req, res) =>{
+            const email = req.params.email;
+            const filter = {email: email};
+            const updateDoc = {
+                $set: {role:'admin'},
+            };
+            const result = await userCollection.updateOne(filter,updateDoc);
+            res.send(result)
+        })
+
+            // -------------------admin  Delete method ------------------
+            app.delete('/user/:id', async(req, res) =>{
+                const id = req.params.id;
+                const query = {_id: ObjectId(id)};
+                const result = await userCollection.deleteOne(query)
+                res.send(result);
+              }) 
+
+            //   ----------------get admin method-------------
+            app.get('/admin/:email', async(req, res) =>{
+                const email = req.params.email;
+                const user = await userCollection.findOne({email: email});
+                const isAdmin = user.role === 'admin';
+                res.send({admin: isAdmin})
+            })
+           
+
      //-------------- User Profile end-----------------------
 
 
